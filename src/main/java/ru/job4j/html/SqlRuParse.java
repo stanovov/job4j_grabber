8546d0790b3fbb38631110a530af2bb3376a320c
Semyon Stanovov <sestanovov@gmail.com>
2021-07-26 10:38:00 +0300

    2.4. SqlRuParse [#285213]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -4,6 +4,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.Parse;
 import ru.job4j.grabber.Post;
 import ru.job4j.grabber.utils.DateTimeParser;
 import ru.job4j.grabber.utils.SqlRuDateTimeParser;
@@ -11,41 +12,70 @@ import ru.job4j.grabber.utils.SqlRuDateTimeParser;
 import java.io.IOException;
 import java.text.ParseException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
-public class SqlRuParse {
+public class SqlRuParse implements Parse {
 
-    private static DateTimeParser timeParser = new SqlRuDateTimeParser();
+    private final DateTimeParser dateTimeParser;
 
-    public static void main(String[] args) throws Exception {
-        String url = "https://www.sql.ru/forum/job-offers";
-        for (int page = 1; page <= 5; page++) {
-            Document doc = Jsoup.connect(String.format("%s/%d", url, page)).get();
-            Elements row = doc.select(".postslisttopic");
-            for (Element td : row) {
-                System.out.println(parseVacancy(td));
-            }
-        }
+    public SqlRuParse(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
     }
 
-    public static Post parseVacancy(Element td) {
-        Element elemVacancy = td.child(0);
-        String link = elemVacancy.attr("href");
-        String title = elemVacancy.text();
-        try {
-            Document docPost = Jsoup.connect(link).get();
-            Element msgTable = docPost.select(".msgTable").first();
-            String description = msgTable.child(0).child(1).child(1).text();
-            String dataTime = msgTable.child(0).child(2).child(0).text();
-            dataTime = dataTime.substring(0, dataTime.indexOf("[") - 1);
-            LocalDateTime created;
+    @Override
+    public List<Post> list(String link) {
+        List<Post> result = new ArrayList<>();
+        int limit = 1;
+        for (int page = 1; page <= limit; page++) {
+            String url = String.format("%s/%d", link, page);
+            Document doc;
             try {
-                created = timeParser.parse(dataTime);
-            } catch (ParseException e) {
+                doc = Jsoup.connect(url).get();
+            } catch (IOException e) {
                 throw new RuntimeException(e);
             }
-            return new Post(title, link, description, created);
+            Elements elements = doc.select(".postslisttopic");
+            List<Element> elemPosts = elements.stream()
+                    .filter(element -> !element.text().startsWith("Важно: ") && !element.text().contains("[закрыт]"))
+                    .collect(Collectors.toList());
+            for (Element elemPost : elemPosts) {
+                String postLink = elemPost.child(0).attr("href");
+                Post post = detail(postLink);
+                result.add(post);
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public Post detail(String link) {
+        Document doc;
+        try {
+            doc = Jsoup.connect(link).get();
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+        Element msgTable = doc.select(".msgTable").first().child(0);
+        String title = msgTable.child(0).child(0).text();
+        String description = msgTable.child(1).child(1).text();
+        String dataTime = msgTable.child(2).child(0).text();
+        dataTime = dataTime.substring(0, dataTime.indexOf("[") - 1);
+        LocalDateTime created;
+        try {
+            created = dateTimeParser.parse(dataTime);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+        return new Post(title, link, description, created);
+    }
+
+    public static void main(String[] args) {
+        String url = "https://www.sql.ru/forum/job-offers";
+        SqlRuParse sqlRuParse = new SqlRuParse(new SqlRuDateTimeParser());
+        for (Post post : sqlRuParse.list(url)) {
+            System.out.println(post);
         }
     }
 }
