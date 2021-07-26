078b678794f720eca34b8f9f650ea9a5d231f0a7
Semyon Stanovov <sestanovov@gmail.com>
2021-07-26 09:07:25 +0300

    2.2. Модель данных - Post. [#285211 #178424] Исправление

--- src/main/java/ru/job4j/grabber/Post.java
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -80,13 +80,12 @@ public class Post {
         return id == post.id
                 && Objects.equals(title, post.title)
                 && Objects.equals(link, post.link)
-                && Objects.equals(description, post.description)
                 && Objects.equals(created, post.created);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, title, link, description, created);
+        return Objects.hash(id, title, link, created);
     }
 
     @Override
